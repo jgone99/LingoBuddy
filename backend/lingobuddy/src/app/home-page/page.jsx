c490940ee8ff14f820a64774be576924e18b90ec
joshh <joshuahinojognzlz@gmail.com>
2024-03-01 23:22:10 -0500

    clerk auth

---
+++ backend/lingobuddy/src/app/home-page/page.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+        <>
+            <h1 className="text-2xl font-bold mb-5">Home Page</h1>
+            <p className="mb-5">Welcome to the Home Page (Logged In)</p>
+        </>
+    )
+};
+
+export default HomePage;
