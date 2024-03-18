7cdbd96026ae59b2e589236784a35ed081a4a1a3
joshh <joshuahinojognzlz@gmail.com>
2024-03-18 18:34:37 -0400

    changed naming convention

--- src/app/quiz/page.jsx
+++ src/app/quiz/page.jsx
@@ -1,5 +1,5 @@
 // pages/quiz.jsx
-import QuizManager from "../src/app/components/learning-course/quiz-manager";
+import QuizManager from "../components/learning-course/quiz-manager";
 
 const QuizPage = () => {
   return <QuizManager />;
