a2ad99343c1511331e864c526febb9cdf4721e04
joshh <joshuahinojognzlz@gmail.com>
2024-03-23 21:54:06 -0400

    more matching-game logic

--- src/app/components/matching/matching-game.jsx
+++ src/app/components/matching/matching-game.jsx
@@ -3,62 +3,78 @@
 import { useEffect, useState } from 'react'
 import Connection from '../../components/matching/lines'
 
-const MatchingGame = ({ matches }) => {
-    const [ firstWordSelected, setFirstWordSelected ] = useState(false)
-    const [ secondWordSelected, setSecondWordSelected ] = useState(false)
-    const [ firstWord, setFirstWord ] = useState()
-    const [ secondWord, setSecondWord ] = useState()
+const english_lang = 'english'
+const spanish_lang = 'spanish'
+
+const MatchingGame = ({ matches, orders }) => {
+    const [ firstWord, setFirstWord ] = useState(null)
+    const [ matchesFound, setMatchesFound ] = useState(0)
 
     const matchCards = () => {
-        console.log(matches)
+        const matchesArray = [...matches]
+        const ordersArray = [...orders]
+        //matchesArray.forEach((match) => console.log(match));
+        //ordersArray.forEach((order) => console.log(order));
+        var cards = []
+        ordersArray.forEach((order, index) => {
+            cards.push(
+                <div key={`english-word-${index}`} content={order[0]} word-lang={english_lang} className={`word-card text-center content-center col-start-1 row-start-${index+1} block w-28 h-16 border rounded hover:shadow-lg bg-cyan-500 bg-opacity-30`} onClick={buttonClick}>
+                    {order[0]}
+                </div>)
+            cards.push(
+                <div key={`spanish-word-${index}`} content={order[1]} word-lang={spanish_lang} className={`word-card text-center content-center col-start-4 row-start-${index+1} block w-28 h-16 border rounded hover:shadow-lg bg-cyan-500 bg-opacity-30`} onClick={buttonClick}>
+                    {order[1]}
+                </div>
+            )
+        })
+        return cards
     }
 
     const buttonClick = (e) => {
         console.log('click')
         const wordCard = e.target
-        if (firstWordSelected) {
-            if (firstWord.getAttribute('content') === wordCard.getAttribute('content')) {
-                firstWord.classList.replace('bg-cyan-500', 'bg-green-500')
-                wordCard.classList.replace('bg-cyan-500', 'bg-green-500')
-            }
-            else {
-                firstWord.classList.replace('bg-green-500', 'bg-cyan-500')
-            }
+        var matchFound = false
+        if (firstWord !== null) {
+            if (wordCard.getAttribute('word-lang') !== firstWord.getAttribute('word-lang')) {
+                matches.forEach((match) => {
+                    const isMatch = (firstWord.getAttribute('content') == match[0] && wordCard.getAttribute('content')) == match[1] || (wordCard.getAttribute('content') == match[0] && firstWord.getAttribute('content') == match[1])
+                    console.log(isMatch)
+                    matchFound = matchFound || isMatch
+                })
+                if (matchFound) {
+                    wordCard.classList.replace('bg-cyan-500', 'bg-green-500')
+                    firstWord.classList.add('pointer-events-none')
+                    wordCard.classList.add('pointer-events-none')
+                    setMatchesFound(matchesFound+1)
+                }
+                else {
+                    firstWord.classList.replace('bg-green-500', 'bg-cyan-500')
+                }
                 setFirstWord(null)
-                setFirstWordSelected(false)
+            }
         }
         else {
             setFirstWord(wordCard)
             wordCard.classList.replace('bg-cyan-500', 'bg-green-500')
-            setFirstWordSelected(true)
         }
     }
 
-    const resetMatching = () => {
+    const resetAll = () => {
         console.log('reset')
         setFirstWord(null)
-        firstWord.classList.replace('bg-green-500', 'bg-cyan-500')
-        setFirstWordSelected(false)
+        setMatchesFound(0)
+        document.querySelectorAll('.word-card').forEach((card) => {
+            card.classList.replace('bg-green-500', 'bg-cyan-500')
+        })
     }
 
     return (
         <>
-            {matchCards()}
+            <div>{matchesFound}</div>
             <div className="container mx-auto my-8" >
                 <svg id='connections'></svg>
-                <div className="grid grid-cols-6 gap-4">
-                    <div id='word1'content='a' className="col-start-1 row-start-1 block p-4 border rounded hover:shadow-lg bg-cyan-500 bg-opacity-30" draggable='true' onClick={buttonClick}>
-                        <h3 id='word1' className='pointer-events-none'>a</h3>
-                    </div>
-                    <div id='word2' content='b' className="col-start-6 row-start-1 block p-4 border rounded hover:shadow-lg bg-cyan-500 bg-opacity-30" draggable='true' onClick={buttonClick}>
-                        <h3 className='pointer-events-none'>b</h3>
-                    </div>
-                    <div id='word3' content='b' className="col-start-1 row-start-2 block p-4 border rounded hover:shadow-lg bg-cyan-500 bg-opacity-30" draggable='true' onClick={buttonClick}>
-                        <h3 id='word1' className='pointer-events-none'>b</h3>
-                    </div>
-                    <div id='word4' content='a' className="col-start-6 row-start-2 block p-4 border rounded hover:shadow-lg bg-cyan-500 bg-opacity-30" draggable='true' onClick={buttonClick}>
-                        <h3 className='pointer-events-none'>a</h3>
-                    </div>
+                <div className="grid grid-cols-4 gap-4">
+                    {matchCards()}
                 </div>
             </div>
         </>
--- src/app/games/matching/page.jsx
+++ src/app/games/matching/page.jsx
@@ -4,18 +4,53 @@ import { query } from '../../db/queries'
 
 const MatchingPage = async() => {
     const matchCount = 5
-
+    var wordMatches = []
+    var wordOrder = []
     const queryWords = async() => {
-        const ans = (await query(`SELECT * FROM word_pairs ORDER BY RANDOM() LIMIT ${matchCount}`)).map((match) => [match['english'], match['spanish']])
-        //console.log(ans)
-        return ans;
+        const ans = (await query(
+            `WITH words_num AS (
+                SELECT *, ROW_NUMBER() OVER(ORDER BY RANDOM()) AS num
+                FROM (
+                    SELECT *
+                    FROM word_pairs 
+                    ORDER BY RANDOM()
+                    LIMIT ${matchCount}
+                ) words
+            ), english_rand AS (
+                SELECT *, ROW_NUMBER() OVER(ORDER BY RANDOM()) AS num
+                FROM (
+                    SELECT english
+                    FROM words_num
+                ) english
+            ), spanish_rand AS (
+                SELECT *, ROW_NUMBER() OVER(ORDER BY RANDOM()) AS num
+                FROM (
+                    SELECT spanish
+                    FROM words_num
+                ) spanish
+            )
+            SELECT a.english, a.spanish, b.english AS english_rand, c.spanish AS spanish_rand
+            FROM words_num a
+            RIGHT JOIN
+            english_rand b
+            ON a.num = b.num
+            RIGHT JOIN
+            spanish_rand c
+            ON a.num = c.num;`
+        )).forEach((match) => {
+            wordMatches.push([match['english'], match['spanish']])
+            wordOrder.push([match['english_rand'], match['spanish_rand']])
+        })
+        // console.log(wordMatches)
+        // console.log(wordOrder)
+        //return ans;
     }
 
-    const data = await queryWords()
+    await queryWords()
 
     return (
         <>
-            <MatchingGame matches={data}/>
+            <MatchingGame matches={wordMatches} orders={wordOrder}/>
         </>
     )
 }
