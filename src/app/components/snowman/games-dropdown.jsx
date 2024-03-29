a9e1c8ada4323759edb6f8e533281e88b5ba2556
joshh <joshuahinojognzlz@gmail.com>
2024-03-28 23:26:36 -0400

    fixed header css

--- src/app/components/snowman/games-dropdown.jsx
+++ src/app/components/snowman/games-dropdown.jsx
@@ -15,8 +15,8 @@ const GamesDropdown = () => {
       };
 
     return (
-        <div className='flex-1 bg-inherit text-center h-full' onMouseLeave={handleMouseLeave}>
-            <button className="h-full hover:text-gray-300 cursor-pointer bg-inherit" onMouseEnter={handleMouseEnter}>
+        <div className='flex-1 bg-inherit games-tab text-center h-full' onMouseLeave={handleMouseLeave}>
+            <button className="h-full text-inherit hover:text-gray-300 cursor-pointer bg-inherit" onMouseEnter={handleMouseEnter}>
                 Games
             </button>
             {visible && (
