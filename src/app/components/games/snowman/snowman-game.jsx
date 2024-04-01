e48b099857a3b34924a47cbcdb561dc4ae18fc11
joshh <joshuahinojognzlz@gmail.com>
2024-04-01 02:26:53 -0400

    making snowman responsive and adding smooth transitions

--- src/app/components/games/snowman/snowman-game.jsx
+++ src/app/components/games/snowman/snowman-game.jsx
@@ -4,11 +4,14 @@ import { useState, useEffect, useTransition } from 'react'
 import SnowmanFigure from "./snowman-figure"
 import style from '../../../components/games/snowman/snowman.css'
 import Modal from '../modal'
+import { getEventListeners } from 'events'
 
 const alphabetArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "á", "é", "í", "ó", "ú"]
 var won = false
 var gamesWon = 0
 var testLoading = false
+var dataFadeOutDone = false
+var gbFadeOutDone = false
 const maxErrors = 4
 
 const SnowmanGame = ({
@@ -29,6 +32,21 @@ const SnowmanGame = ({
     const [currentHighscore, setCurrentHighscore] = useState(highscore)
     const [isPending, startTransition] = useTransition()
 
+    const maxScreenWidth = screen.width
+    const maxScreenHeight = screen.height
+
+    const resize = () => {
+        const figureElement = document.querySelector('#figure-id')
+        const screenWidth = window.innerWidth
+        const screenHeight = window.innerHeight
+
+        if (screenWidth > 600) {
+            figureElement.style.transform = `scale(${1.2 * Math.min(screenWidth / maxScreenWidth, screenHeight / maxScreenHeight)})`
+        }
+    }
+
+    window.addEventListener("resize", resize)
+
     const guessBoxes = () => {
         return String(word['spanish']).split('').map((letter, index) => {
             return <div key={letter + index} className={`flex mx-1 border-solid items-center justify-center rounded border-2 w-8 h-8 border-cyan-500 guessbox letter-${letter} is-empty`}></div>
@@ -43,9 +61,7 @@ const SnowmanGame = ({
         })
     }
 
-    const playAgain = () => {
-        testLoading = true;
-
+    const fetchAll = () => {
         Promise.all([getHighscore(), getNewWord()]).then(result => {
             setCurrentHighscore(result[0])
             setWord(result[1])
@@ -53,8 +69,21 @@ const SnowmanGame = ({
             setCorrectLetterCount(0)
             setErrorCount(0)
             resetGuessBoxes()
-            testLoading = false
+            guessboxesFade(false)
+            setFade(data, false, false, null, null)
         })
+    }
+
+    const playAgain = () => {
+        const data = document.querySelector('#data')
+        const guessboxes = document.querySelector('#guess-boxes')
+        setFade(data, true, false, null, null)
+        setFade(guessboxes, true, true, function callFetchAll(args, e, func) {
+            console.log(e)
+            guessboxes.removeEventListener(e.type, func)
+            fetchAll()
+        }, null)
+        
     }
 
     const alphabetButtons = () => {
@@ -94,6 +123,11 @@ const SnowmanGame = ({
         setCorrectLetterCount(correctLetterCount + emptyBoxes.length)
     }
 
+    const guessboxesFade = (toFadeOut) => {
+        const guessboxes = document.querySelector('#guess-boxes')
+        setFade(guessboxes, toFadeOut)
+    }
+
     const errorMade = () => {
         setErrorCount(errorCount + 1)
         if (errorCount + 1 >= maxErrors) {
@@ -102,9 +136,69 @@ const SnowmanGame = ({
         }
     }
 
+    const fadeOut = (element) => {
+        if (element.classList.replace('fade-in', 'fade-out')) {
+            console.log('replaced')
+        } else {
+            element.classList.add('fade-out')
+            console.log('added')
+        }
+
+    }
+
+    const fadeIn = (element) => {
+        if (element.classList.replace('fade-out', 'fade-in')) {
+            console.log('replaced')
+        } else {
+            element.classList.add('fade-in')
+            console.log('added')
+        }
+
+    }
+
+    const setFade = (element, toFadeOut, withListener, func, args) => {
+        if (withListener) {
+            element.addEventListener('animationend', function callbackFunc(e) {
+                if (e.animationName == (toFadeOut ? 'fadeOut' : 'fadeIn')) {
+                    console.log(`${toFadeOut ? 'fadeOut' : 'fadeIn'} ended`)
+                    func(args, e, callbackFunc)
+                }
+            })
+        }
+
+        toFadeOut ? fadeOut(element) : fadeIn(element)
+    }
+
+    const modalFade = (toFadeOut) => {
+        const modal = document.querySelector('.games-modal')
+        console.log(modal)
+        modal.addEventListener('animationend', (e) => {
+            console.log('animationend')
+            console.log(e)
+            if (e.animationName == 'fadeOut') {
+                console.log('fade out ended')
+                setShowModal(false)
+            }
+        })
+        setFade(modal, toFadeOut)
+    }
+
+    const resetSnowman = () => {
+        const nose = document.querySelector('#nose')
+        const leftHand = document.querySelector('#left-hand')
+        const rightHand = document.querySelector('#right-hand')
+        const head = document.querySelector('#head')
+        nose.classList.add('nose-fall-back')
+        leftHand.classList.add('left-hand-fall-back')
+        rightHand.classList.add('right-hand-fall-back')
+        head.classList.add('head-fall-back')
+    }
+
     const modalContinue = () => {
+        modalFade(true)
+        resetSnowman()
+        console.log('play again')
         playAgain()
-        setShowModal(false)
     }
 
 
@@ -116,27 +210,30 @@ const SnowmanGame = ({
     }
     // FOR TESTING
 
-    return testLoading ? 'Loading...' : (
+    return (
         <>
-            <div className='game-container'>
+            <div className='game-container bg-black'>
                 {showModal && <Modal won={won} isPending={isPending} modalContinue={modalContinue} />}
-                <div>HIGHSCORE: {currentHighscore}</div>
-                <div>SCORE: {gamesWon}</div>
-                <div className='my-10'>
-                    <h4>WORD: {word['english'].toUpperCase()}</h4>
+                <div id='data'>
+                    <div>HIGHSCORE: {currentHighscore}</div>
+                    <div>SCORE: {gamesWon}</div>
+                    <div className='my-10'>
+                        <h4>WORD: {word['english'].toUpperCase()}</h4>
+                    </div>
                 </div>
+
                 {/* FOR TESTING */}
                 <button onClick={resetUser}>RESET USER (TESTING)</button>
                 {/* FOR TESTING */}
                 <div className='main-comps'>
-                    <div className='figure'>
+                    <div id='figure-id' className='figure'>
                         <div>
                             <SnowmanFigure errors={errorCount} />
                         </div>
                     </div>
-                    <div className='interactive'>
+                    <div id='interactive' className='interactive'>
                         <div className='w-96 mt-20'>
-                            <div className='mb-20 flex justify-center'>
+                            <div id='guess-boxes' className='mb-20 flex justify-center guess-boxes'>
                                 {guessBoxes()}
                             </div>
                             <div className="h-full">
