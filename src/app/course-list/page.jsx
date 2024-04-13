6ad4727e047ae1891d70b88f2d4ded2ef5d1ded4
joshh <joshuahinojognzlz@gmail.com>
2024-04-13 16:29:45 -0400

    added courses try again db update check

--- src/app/course-list/page.jsx
+++ src/app/course-list/page.jsx
@@ -10,9 +10,25 @@ const userProgressTableName = "course_progress";
 export default async function CourseListPage() {
   const { userId } = auth();
 
+  const fetchUserProgress = async () => {
+    "use server";
+    const res = await query("SELECT * FROM course_progress WHERE user_id=$1", [
+      userId,
+    ]);
+    return res[0];
+  };
+
   async function updateUserProgress(levelId, sectionId) {
     "use server";
     console.log("updating user");
+
+    const ans = await fetchUserProgress()
+
+    if(ans.level_id > levelId || (ans.level_id == levelId && ans.section_id >= sectionId)) {
+      console.log('did not update. saved progression is ahead')
+      return
+    }
+
     const updateUserQuery = `UPDATE course_progress
 			SET level_id = $2, section_id = $3
 			WHERE user_id = $1`;
@@ -40,14 +56,6 @@ export default async function CourseListPage() {
     return res;
   };
 
-  const fetchUserProgress = async () => {
-    "use server";
-    const res = await query("SELECT * FROM course_progress WHERE user_id=$1", [
-      userId,
-    ]);
-    return res[0];
-  };
-
   const userProgress = await fetchUserProgress();
 
   const allQuestions = await fetchAllQuestions();
@@ -68,7 +76,6 @@ export default async function CourseListPage() {
 
   return (
     <>
-      {console.log(userProgress)}
       <CourseListComponent
         initUserProgress={userProgress}
         fetchUserProgress={fetchUserProgress}
