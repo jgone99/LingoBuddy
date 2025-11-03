9a4e2623a32363bdfdce5bc753c2b867422c3fcb
joshh <joshuahinojognzlz@gmail.com>
2025-11-03 06:23:26 -0500

    fixed signout redirect issues

--- src/app/components/client-wrapper.jsx
+++ src/app/components/client-wrapper.jsx
@@ -4,7 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs"
 
 export default function ClerkWrapper({ children }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider afterSignOutUrl={process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_OUT_URL}>
       {children}
     </ClerkProvider>
   )
